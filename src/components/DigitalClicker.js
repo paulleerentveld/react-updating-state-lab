b6dbb4a27b9472a684afdd739fa37c0142d70981
paulleerentveld <paul@veldnet.com.au>
2022-05-09 16:24:37 +1000

    lab stuff

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,30 @@
 // Code DigitalClicker Component Here
+import React, { Component } from "react";
+
+export default class DigitalClicker extends Component {
+
+    constructor() {
+        super();
+    
+        // Define the initial state:
+        this.state = {
+            timesClicked: 0,
+        };
+      }
+
+      updateClicked = () => {
+        this.setState({
+            timesClicked: this.state.timesClicked +1
+        })
+      }
+
+
+  render() {
+    return <div>
+            <button onClick={this.updateClicked} label={this.state.timesClicked} >{this.state.timesClicked}</button>
+    </div>
+    
+    
+    
+  }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,55 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from "react";
+
+export default class YouTubeDebugger extends Component {
+
+    constructor() {
+        super();
+    
+        // Define the initial state:
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                    resolution: '1080p'
+                }
+            }
+        };
+      }
+
+      changeResolution = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                video: {
+                    resolution: '720p'
+                }
+            }
+
+        })
+      }
+
+      changeBitrate = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                bitrate: 12,
+            }
+
+        })
+      }
+
+
+  render() {
+    return <div>
+            <button className="resolution" onClick={this.changeResolution} >Change Resolution</button>
+            
+            <button className="bitrate" onClick={this.changeBitrate} >Change bitrate</button>
+    </div>
+    
+    
+    
+  }
+}
